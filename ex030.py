cf5767fabc42c78c6d28cada3ced2811cef40361
Tiago Angeli <102192008+AgenteDeveloper@users.noreply.github.com>
2024-04-01 21:49:24 -0300

    Diferenciador de Impar e Par
    Leitor básico de números que entrega se o número é impar ou par

---
+++ ex030.py
@@ -0,0 +1,9 @@
+# Diferenciador de Par ou Impar
+
+numero = int(input("Insira um numero qualquer: "))
+
+if numero % 2 == 0:
+    print("O numero {} é par".format(numero))
+
+else:
+    print("O numero {} é ímpar".format(numero))
